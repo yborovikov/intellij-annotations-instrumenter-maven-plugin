c40bd2c74c456ca8698f61a363551f0a5b479baf
Olle Sundblad <olle@eris.se>
2017-04-08 00:04:19 +0200

    cleanup in tests

--- src/test/java/se/eris/notnull/AnnotationNotNullInstrumenterTest.java
+++ src/test/java/se/eris/notnull/AnnotationNotNullInstrumenterTest.java
@@ -24,13 +24,13 @@ import org.junit.rules.ExpectedException;
 import se.eris.maven.NopLogWrapper;
 import se.eris.notnull.instrumentation.ClassMatcher;
 import se.eris.util.ReflectionUtil;
+import se.eris.util.TestClass;
 import se.eris.util.TestCompiler;
 
 import java.io.File;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.file.Path;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -42,9 +42,9 @@ import static org.hamcrest.Matchers.greaterThan;
 public class AnnotationNotNullInstrumenterTest {
 
     private static final File SRC_DIR = new File("src/test/data");
-    private static final Path CLASSES_DIRECTORY = new File("target/test/data/classes").toPath();
+    private static final File TARGET_DIR = new File("target/test/data/classes");
 
-    private static final File TEST_FILE = new File(SRC_DIR, "se/eris/test/TestNotNull.java");
+    private static final TestClass TEST_CLASS = new TestClass("se.eris.test.TestNotNull");
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
@@ -53,12 +53,12 @@ public class AnnotationNotNullInstrumenterTest {
 
     @BeforeClass
     public static void beforeClass() throws MalformedURLException {
-        compiler = TestCompiler.create(CLASSES_DIRECTORY);
-        compiler.compile(TEST_FILE);
+        compiler = TestCompiler.create(TARGET_DIR.toPath());
+        compiler.compile(TEST_CLASS.getFile(SRC_DIR));
 
         final Configuration configuration = new Configuration(false, new AnnotationConfiguration(notNull(), Collections.<String>emptySet()), new ExcludeConfiguration(Collections.<ClassMatcher>emptySet()));
         final NotNullInstrumenter instrumenter = new NotNullInstrumenter(new NopLogWrapper());
-        final int numberOfInstrumentedFiles = instrumenter.addNotNullAnnotations(CLASSES_DIRECTORY, configuration, Collections.<URL>emptyList());
+        final int numberOfInstrumentedFiles = instrumenter.addNotNullAnnotations(TARGET_DIR.toPath(), configuration, Collections.<URL>emptyList());
 
         assertThat(numberOfInstrumentedFiles, greaterThan(0));
     }
@@ -73,46 +73,46 @@ public class AnnotationNotNullInstrumenterTest {
 
     @Test
     public void annotatedParameter_shouldValidate() throws Exception {
-        final Class<?> c = compiler.getCompiledClass("se.eris.test.TestNotNull");
+        final Class<?> c = compiler.getCompiledClass(TEST_CLASS.getName());
         final Method notNullParameterMethod = c.getMethod("notNullParameter", String.class);
         ReflectionUtil.simulateMethodCall(notNullParameterMethod, "should work");
 
         exception.expect(IllegalArgumentException.class);
-        exception.expectMessage("Argument 0 for @NotNull parameter of se/eris/test/TestNotNull.notNullParameter must not be null");
+        exception.expectMessage("Argument 0 for @NotNull parameter of " + TEST_CLASS.getMessageName() + ".notNullParameter must not be null");
         ReflectionUtil.simulateMethodCall(notNullParameterMethod, new Object[]{null});
     }
 
     @Test
     public void notnullReturn_shouldValidate() throws Exception {
-        final Class<?> c = compiler.getCompiledClass("se.eris.test.TestNotNull");
+        final Class<?> c = compiler.getCompiledClass(TEST_CLASS.getName());
         final Method notNullReturnMethod = c.getMethod("notNullReturn", String.class);
         ReflectionUtil.simulateMethodCall(notNullReturnMethod, "should work");
 
         exception.expect(IllegalStateException.class);
-        exception.expectMessage("NotNull method se/eris/test/TestNotNull.notNullReturn must not return null");
+        exception.expectMessage("NotNull method " + TEST_CLASS.getMessageName() + ".notNullReturn must not return null");
         ReflectionUtil.simulateMethodCall(notNullReturnMethod, new Object[]{null});
     }
 
     @Test
     public void annotatedReturn_shouldValidate() throws Exception {
-        final Class<?> c = compiler.getCompiledClass("se.eris.test.TestNotNull");
+        final Class<?> c = compiler.getCompiledClass(TEST_CLASS.getName());
         final Method notNullReturnMethod = c.getMethod("annotatedReturn", String.class);
         ReflectionUtil.simulateMethodCall(notNullReturnMethod, "should work");
 
         exception.expect(IllegalStateException.class);
-        exception.expectMessage("NotNull method se/eris/test/TestNotNull.annotatedReturn must not return null");
+        exception.expectMessage("NotNull method " + TEST_CLASS.getMessageName() + ".annotatedReturn must not return null");
         ReflectionUtil.simulateMethodCall(notNullReturnMethod, new Object[]{null});
     }
 
     @Test
     public void overridingMethod_isInstrumented() throws Exception {
-        final Class<?> subargClass = compiler.getCompiledClass("se.eris.test.TestNotNull$Subarg");
-        final Class<?> subClass = compiler.getCompiledClass("se.eris.test.TestNotNull$Sub");
+        final Class<?> subargClass = compiler.getCompiledClass(TEST_CLASS.getName() +"$Subarg");
+        final Class<?> subClass = compiler.getCompiledClass(TEST_CLASS.getName() +"$Sub");
         final Method specializedMethod = subClass.getMethod("overload", subargClass);
         assertFalse(specializedMethod.isSynthetic());
         assertFalse(specializedMethod.isBridge());
         exception.expect(IllegalArgumentException.class);
-        exception.expectMessage("Argument 0 for @NotNull parameter of se/eris/test/TestNotNull$Sub.overload must not be null");
+        exception.expectMessage("Argument 0 for @NotNull parameter of " + TEST_CLASS.getMessageName() + "$Sub.overload must not be null");
         ReflectionUtil.simulateMethodCall(subClass.newInstance(), specializedMethod, new Object[]{null});
     }
 
